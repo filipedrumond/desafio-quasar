168cda4fbe71418769f7a42d15ee4101bb7e78a3
Filipe Drumond Pereira <filipe.dp@outlook.com>
2021-09-30 00:55:59 -0300

    :bug: correção sessionID alterava

--- src/helpers/sessionHanddler.js
+++ src/helpers/sessionHanddler.js
@@ -2,18 +2,18 @@ import Cookies from 'js-cookie'
 import makeid from './makeId'
 
 export default function sessionHanddler (store) {
-  const newSsid = makeid(18)
   const ssidStore = store.getters['session/getsessionID']
   const ssidCookie = Cookies.get('sessionID')
 
-  if (ssidCookie === undefined && ssidStore !== '') {
-    Cookies.set('sessionID', ssidStore)
-    return
-  }
   if (ssidCookie !== undefined && ssidStore === '') {
     store.dispatch('session/setSessionIdAction', ssidCookie)
-    return
   }
-  store.dispatch('session/setSessionIdAction', newSsid)
-  Cookies.set('sessionID', newSsid)
+  if (ssidCookie === undefined && ssidStore !== '') {
+    Cookies.set('sessionID', ssidStore)
+  }
+  if (ssidCookie === undefined && ssidStore === '') {
+    const newSsid = makeid(18)
+    store.dispatch('session/setSessionIdAction', newSsid)
+    Cookies.set('sessionID', newSsid)
+  }
 }
